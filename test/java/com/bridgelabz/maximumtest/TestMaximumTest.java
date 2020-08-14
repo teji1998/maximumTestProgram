e5fb4e024bbd14f8c7af3fa5af826f32d140cf35
“teji1998” <tejasvinirpk@gmail.com>
2020-08-14 05:45:34 +0530

    Added:Function to find the maximum in three integers.

--- test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
+++ test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.maximumtest;
 
-public class TestMaximumTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestMaximumTest {
+	//Maximum Value At First Position
+	@Test
+	public void Maximum_Value_At_First_Position() {
+		TestMaximum maximumNumber = new TestMaximum(9, 7, 5);
+		int maximum = maximumNumber.numberHavingMaximumValue();
+		Assert.assertEquals(9, maximum);
+	}
+
+	//Maximum Value At Second Position
+	@Test
+	public void Maximum_Value_At_Second_Position() {
+		TestMaximum maximumNumber = new TestMaximum(7, 9, 5);
+		int maximum = maximumNumber.numberHavingMaximumValue();
+		Assert.assertEquals(9, maximum);
+	}
+
+	//Maximum Value At Third Position
+	@Test
+	public void Maximum_Value_At_Third_Position() {
+		TestMaximum maximumNumber = new TestMaximum(7, 5, 9);
+		int maximum = maximumNumber.numberHavingMaximumValue();
+		Assert.assertEquals(9, maximum);
+	}
 }
+
