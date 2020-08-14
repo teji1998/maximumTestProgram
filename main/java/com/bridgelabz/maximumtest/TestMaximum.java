1116a8081c12f30c7e82c2abe0c3b09f9bbe159c
“teji1998” <tejasvinirpk@gmail.com>
2020-08-14 15:05:17 +0530

    Refactored:Used only one generic class

--- main/java/com/bridgelabz/maximumtest/TestMaximum.java
+++ main/java/com/bridgelabz/maximumtest/TestMaximum.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.maximumtest;
 
-public class TestMaximum {
-	public <E extends Comparable> E inputHavingMaximumValue(E value1, E value2, E value3){
+public class TestMaximum<E extends Comparable <E>> {
+	E value1,value2, value3;
+
+	TestMaximum(E value1, E value2, E value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	public E testMaximum() {
+		return inputHavingMaximumValue(value1, value2, value3);
+	}
+
+	public static <E extends Comparable> E inputHavingMaximumValue( E value1, E value2 ,E value3){
 		E maximum = value1;
 		if(value2.compareTo(maximum) > 0){
 			maximum = value2;
