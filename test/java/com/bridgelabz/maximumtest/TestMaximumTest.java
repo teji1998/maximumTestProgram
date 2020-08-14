4c272df824989ab7519b2b936833e067ab3b90d8
“teji1998” <tejasvinirpk@gmail.com>
2020-08-14 08:54:03 +0530

    Added:Function to find the maximum in three float numbers.

--- test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
+++ test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
@@ -7,25 +7,50 @@ public class TestMaximumTest {
 	//Maximum Value At First Position
 	@Test
 	public void Maximum_Value_At_First_Position() {
-		TestMaximum maximumNumber = new TestMaximum(9, 7, 5);
-		int maximum = maximumNumber.numberHavingMaximumValue();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = maximumNumber.numberHavingMaximumValue(9, 7, 5);
 		Assert.assertEquals(9, maximum);
 	}
 
 	//Maximum Value At Second Position
 	@Test
 	public void Maximum_Value_At_Second_Position() {
-		TestMaximum maximumNumber = new TestMaximum(7, 9, 5);
-		int maximum = maximumNumber.numberHavingMaximumValue();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = maximumNumber.numberHavingMaximumValue(7, 9, 5);
 		Assert.assertEquals(9, maximum);
 	}
 
 	//Maximum Value At Third Position
 	@Test
 	public void Maximum_Value_At_Third_Position() {
-		TestMaximum maximumNumber = new TestMaximum(7, 5, 9);
-		int maximum = maximumNumber.numberHavingMaximumValue();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = maximumNumber.numberHavingMaximumValue(7, 5, 9);
 		Assert.assertEquals(9, maximum);
 	}
+
+	//Maximum Float Value At First Position
+	@Test
+	public void Maximum_Float_number_At_First_Position() {
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		double maximum = maximumFloatNumber.floatNumberHavingMaximumValue(3.9f, 2.8f, 1.6f);
+		Assert.assertEquals(3.9f, maximum,0);
+	}
+
+	//Maximum Float Value At Second Position
+	@Test
+	public void Maximum_Float_number_At_Second_Position() {
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		double maximum = maximumFloatNumber.floatNumberHavingMaximumValue(1.6f, 3.9f, 2.8f);
+		Assert.assertEquals(3.9f, maximum, 0);
+	}
+
+	//Maximum Float Value At Third Position
+	@Test
+	public void Maximum_Float_number_At_Third_Position() {
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		double maximum = maximumFloatNumber.floatNumberHavingMaximumValue(2.8f, 1.6f, 3.9f);
+		Assert.assertEquals(3.9f, maximum, 0);
+	}
 }
+
 
