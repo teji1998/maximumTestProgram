32e9919fdf2862590414b313817e784c4dfc3d9f
“teji1998” <tejasvinirpk@gmail.com>
2020-08-14 10:27:50 +0530

    Added:Function to find the maximum in three strings.

--- test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
+++ test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
@@ -51,6 +51,30 @@ public class TestMaximumTest {
 		double maximum = maximumFloatNumber.floatNumberHavingMaximumValue(2.8f, 1.6f, 3.9f);
 		Assert.assertEquals(3.9f, maximum, 0);
 	}
+
+	//Maximum String Value At First Position
+	@Test
+	public void Maximum_String_At_First_Position() {
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = maximumString.stringHavingMaximumValue("Zebra","Goat", "Cat");
+		Assert.assertEquals("Zebra", maximum);
+	}
+
+	//Maximum String Value At Second Position
+	@Test
+	public void Maximum_String_At_Second_Position() {
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = maximumString.stringHavingMaximumValue("Goat","Zebra","Cat");
+		Assert.assertEquals("Zebra", maximum);
+	}
+
+	//Maximum String Value At Third Position
+	@Test
+	public void Maximum_String_At_Third_Position() {
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = maximumString.stringHavingMaximumValue("Cat","Goat","Zebra");
+		Assert.assertEquals("Zebra", maximum);
+	}
 }
 
 
