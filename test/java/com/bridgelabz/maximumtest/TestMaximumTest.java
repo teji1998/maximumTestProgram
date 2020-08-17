3a4e2d9e8fea3963ffae37a962d17f5a26c8251a
“teji1998” <tejasvinirpk@gmail.com>
2020-08-17 07:44:59 +0530

    Added:Increased the number of parameters.

--- test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
+++ test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
@@ -8,78 +8,99 @@ public class TestMaximumTest {
 	//Maximum Value At First Position
 	@Test
 	public void Maximum_Value_At_First_Position() {
-		TestMaximum maximumNumber = new TestMaximum(9, 7, 5);
-		int maximum = (int) maximumNumber.testMaximum();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = (int) maximumNumber.testingMaximumValue(9, 7, 1, 5);
 		Assert.assertEquals(9, maximum);
 	}
 
 	//Maximum Value At Second Position
 	@Test
 	public void Maximum_Value_At_Second_Position() {
-		TestMaximum maximumNumber = new TestMaximum(7, 9, 5);
-		int maximum = (int) maximumNumber.testMaximum();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = (int) maximumNumber.testingMaximumValue(7, 9, 5, 1);
 		Assert.assertEquals(9, maximum);
 	}
 
 	//Maximum Value At Third Position
 	@Test
 	public void Maximum_Value_At_Third_Position() {
-		TestMaximum maximumNumber = new TestMaximum(7, 5, 9);
-		int maximum = (int) maximumNumber.testMaximum();
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = (int) maximumNumber.testingMaximumValue(7, 5, 9, 1);
+		Assert.assertEquals(9, maximum);
+	}
+
+	//Maximum Value At Fourth Position
+	@Test
+	public void Maximum_Value_At_Fourth_Position() {
+		TestMaximum maximumNumber = new TestMaximum();
+		int maximum = (int) maximumNumber.testingMaximumValue(1, 7, 5, 9);
 		Assert.assertEquals(9, maximum);
 	}
 
 	//Maximum Float Value At First Position
 	@Test
 	public void Maximum_Float_number_At_First_Position() {
-		TestMaximum maximumFloatNumber = new TestMaximum(3.9f, 2.8f, 1.6f);
-		float maximum = (float) maximumFloatNumber.testMaximum();
-<<<<<<< HEAD
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		float maximum = (float) maximumFloatNumber.testingMaximumValue(3.9f, 2.8f, 1.6f, 2.6f);
 		Assert.assertEquals(3.9f, maximum, 0);
-=======
 		Assert.assertEquals(3.9f, maximum,0);
->>>>>>> UC3_findMaximumString
+
 	}
 
 	//Maximum Float Value At Second Position
 	@Test
 	public void Maximum_Float_number_At_Second_Position() {
-		TestMaximum maximumFloatNumber = new TestMaximum(1.6f, 3.9f, 2.8f);
-		float maximum = (float) maximumFloatNumber.testMaximum();
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		float maximum = (float) maximumFloatNumber.testingMaximumValue(1.6f, 3.9f, 2.6f, 2.8f);
 		Assert.assertEquals(3.9f, maximum, 0);
 	}
 
 	//Maximum Float Value At Third Position
 	@Test
 	public void Maximum_Float_number_At_Third_Position() {
-		TestMaximum maximumFloatNumber = new TestMaximum(2.8f, 1.6f, 3.9f);
-		float maximum = (float) maximumFloatNumber.testMaximum();
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		float maximum = (float) maximumFloatNumber.testingMaximumValue(2.8f, 1.6f, 3.9f, 2.6f);
+		Assert.assertEquals(3.9f, maximum, 0);
+	}
+
+	//Maximum Float Value At Fourth Position
+	@Test
+	public void Maximum_Float_number_At_Fourth_Position() {
+		TestMaximum maximumFloatNumber = new TestMaximum();
+		float maximum = (float) maximumFloatNumber.testingMaximumValue(2.8f, 2.6f, 1.6f, 3.9f);
 		Assert.assertEquals(3.9f, maximum, 0);
 	}
 
 	//Maximum String Value At First Position
 	@Test
 	public void Maximum_String_At_First_Position() {
-		TestMaximum maximumString = new TestMaximum("Zebra","Goat", "Cat");
-		String maximum = (String) maximumString.testMaximum();
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = (String) maximumString.testingMaximumValue("Zebra","Goat", "Cat","Camel");
 		Assert.assertEquals("Zebra", maximum);
 	}
 
 	//Maximum String Value At Second Position
 	@Test
 	public void Maximum_String_At_Second_Position() {
-		TestMaximum maximumString = new TestMaximum("Goat","Zebra","Cat");
-		String maximum = (String) maximumString.testMaximum();
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = (String) maximumString.testingMaximumValue("Goat","Zebra","Cat","Camel");
 		Assert.assertEquals("Zebra", maximum);
 	}
 
 	//Maximum String Value At Third Position
 	@Test
 	public void Maximum_String_At_Third_Position() {
-		TestMaximum maximumString = new TestMaximum("Cat","Goat","Zebra");
-		String maximum = (String) maximumString.testMaximum();
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = (String) maximumString.testingMaximumValue("Cat","Goat","Zebra","Camel");
+		Assert.assertEquals("Zebra", maximum);
+	}
+
+	//Maximum String Value At Fourth Position
+	@Test
+	public void Maximum_String_At_Fourth_Position() {
+		TestMaximum maximumString = new TestMaximum();
+		String maximum = (String) maximumString.testingMaximumValue("Cat","Goat","Camel","Zebra");
 		Assert.assertEquals("Zebra", maximum);
 	}
 }
-
 
