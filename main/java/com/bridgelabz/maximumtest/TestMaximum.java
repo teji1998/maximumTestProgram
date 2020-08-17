4a38fee08021ec938b83004c214b765698b0ee49
“teji1998” <tejasvinirpk@gmail.com>
2020-08-17 08:05:12 +0530

    Added:Method printmax is given to std output.

--- main/java/com/bridgelabz/maximumtest/TestMaximum.java
+++ main/java/com/bridgelabz/maximumtest/TestMaximum.java
@@ -16,4 +16,15 @@ public class TestMaximum < E extends Comparable<E> > {
 		}
 		return maximum;
 	}
+	
+	public static <E extends Comparable<E> > void printMax(){
+		System.out.println("Maximum Integer= " + testingMaximumValue(7, 9, 5, 1));
+		System.out.println("Maximum Float= " + testingMaximumValue(3.9f, 2.8f, 1.6f, 2.6f));
+		System.out.println("Maximum String= "+ testingMaximumValue("Zebra","Goat", "Cat","Camel"));
+	}
+
+	public static void main(String[] args) {
+		printMax();
+	}
+	
 }
