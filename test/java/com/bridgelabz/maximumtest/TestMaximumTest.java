f8022321d58bf96356a83cf75bafab5a2055064e
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 13:58:54 +0530

    Modified:Function name of test cases.

--- test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
+++ test/java/com/bridgelabz/maximumtest/TestMaximumTest.java
@@ -7,7 +7,7 @@ public class TestMaximumTest {
 
 	//Maximum Value At First Position
 	@Test
-	public void Maximum_Value_At_First_Position() {
+	public void givenAnInteger_whenMaximumAtFirstPosition_shouldReturnFirstPosition() {
 		TestMaximum maximumNumber = new TestMaximum();
 		int maximum = (int) maximumNumber.testingMaximumValue(9, 7, 1, 5);
 		Assert.assertEquals(9, maximum);
@@ -15,7 +15,7 @@ public class TestMaximumTest {
 
 	//Maximum Value At Second Position
 	@Test
-	public void Maximum_Value_At_Second_Position() {
+	public void givenAnInteger_whenMaximumAtSecondPosition_shouldReturnSecondPosition() {
 		TestMaximum maximumNumber = new TestMaximum();
 		int maximum = (int) maximumNumber.testingMaximumValue(7, 9, 5, 1);
 		Assert.assertEquals(9, maximum);
@@ -23,7 +23,7 @@ public class TestMaximumTest {
 
 	//Maximum Value At Third Position
 	@Test
-	public void Maximum_Value_At_Third_Position() {
+	public void givenAnInteger_whenMaximumAtThirdPosition_shouldReturnThirdPosition() {
 		TestMaximum maximumNumber = new TestMaximum();
 		int maximum = (int) maximumNumber.testingMaximumValue(7, 5, 9, 1);
 		Assert.assertEquals(9, maximum);
@@ -31,7 +31,7 @@ public class TestMaximumTest {
 
 	//Maximum Value At Fourth Position
 	@Test
-	public void Maximum_Value_At_Fourth_Position() {
+	public void givenAnInteger_whenMaximumAtFourthPosition_shouldReturnFourthPosition() {
 		TestMaximum maximumNumber = new TestMaximum();
 		int maximum = (int) maximumNumber.testingMaximumValue(1, 7, 5, 9);
 		Assert.assertEquals(9, maximum);
@@ -39,7 +39,7 @@ public class TestMaximumTest {
 
 	//Maximum Float Value At First Position
 	@Test
-	public void Maximum_Float_number_At_First_Position() {
+	public void givenAFloat_whenMaximumAtFirstPosition_shouldReturnFirstPosition() {
 		TestMaximum maximumFloatNumber = new TestMaximum();
 		float maximum = (float) maximumFloatNumber.testingMaximumValue(3.9f, 2.8f, 1.6f, 2.6f);
 		Assert.assertEquals(3.9f, maximum, 0);
@@ -47,7 +47,7 @@ public class TestMaximumTest {
 
 	//Maximum Float Value At Second Position
 	@Test
-	public void Maximum_Float_number_At_Second_Position() {
+	public void givenAFloat_whenMaximumAtSecondPosition_shouldReturnSecondPosition() {
 		TestMaximum maximumFloatNumber = new TestMaximum();
 		float maximum = (float) maximumFloatNumber.testingMaximumValue(1.6f, 3.9f, 2.6f, 2.8f);
 		Assert.assertEquals(3.9f, maximum, 0);
@@ -55,7 +55,7 @@ public class TestMaximumTest {
 
 	//Maximum Float Value At Third Position
 	@Test
-	public void Maximum_Float_number_At_Third_Position() {
+	public void givenAFloat_whenMaximumAtThirdPosition_shouldReturnThirdPosition() {
 		TestMaximum maximumFloatNumber = new TestMaximum();
 		float maximum = (float) maximumFloatNumber.testingMaximumValue(2.8f, 1.6f, 3.9f, 2.6f);
 		Assert.assertEquals(3.9f, maximum, 0);
@@ -63,7 +63,7 @@ public class TestMaximumTest {
 
 	//Maximum Float Value At Fourth Position
 	@Test
-	public void Maximum_Float_number_At_Fourth_Position() {
+	public void givenAFloat_whenMaximumAtFourthPosition_shouldReturnFourthPosition() {
 		TestMaximum maximumFloatNumber = new TestMaximum();
 		float maximum = (float) maximumFloatNumber.testingMaximumValue(2.8f, 2.6f, 1.6f, 3.9f);
 		Assert.assertEquals(3.9f, maximum, 0);
@@ -71,7 +71,7 @@ public class TestMaximumTest {
 
 	//Maximum String Value At First Position
 	@Test
-	public void Maximum_String_At_First_Position() {
+	public void givenAString_whenMaximumAtFirstPosition_shouldReturnFirstPosition() {
 		TestMaximum maximumString = new TestMaximum();
 		String maximum = (String) maximumString.testingMaximumValue("Zebra","Goat", "Cat","Camel");
 		Assert.assertEquals("Zebra", maximum);
@@ -79,7 +79,7 @@ public class TestMaximumTest {
 
 	//Maximum String Value At Second Position
 	@Test
-	public void Maximum_String_At_Second_Position() {
+	public void givenAString_whenMaximumAtSecondPosition_shouldReturnSecondPosition() {
 		TestMaximum maximumString = new TestMaximum();
 		String maximum = (String) maximumString.testingMaximumValue("Goat","Zebra","Cat","Camel");
 		Assert.assertEquals("Zebra", maximum);
@@ -87,7 +87,7 @@ public class TestMaximumTest {
 
 	//Maximum String Value At Third Position
 	@Test
-	public void Maximum_String_At_Third_Position() {
+	public void givenAString_whenMaximumAtThirdPosition_shouldReturnThirdPosition() {
 		TestMaximum maximumString = new TestMaximum();
 		String maximum = (String) maximumString.testingMaximumValue("Cat","Goat","Zebra","Camel");
 		Assert.assertEquals("Zebra", maximum);
@@ -95,7 +95,7 @@ public class TestMaximumTest {
 
 	//Maximum String Value At Fourth Position
 	@Test
-	public void Maximum_String_At_Fourth_Position() {
+	public void givenAString_whenMaximumAtFourthPosition_shouldReturnFourthPosition() {
 		TestMaximum maximumString = new TestMaximum();
 		String maximum = (String) maximumString.testingMaximumValue("Cat","Goat","Camel","Zebra");
 		Assert.assertEquals("Zebra", maximum);
